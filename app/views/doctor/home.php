4e257ad19655b327e08f61a0aa13c7509cb84e7c
brendaguerrero9 <00024317@uca.edu.sv>
2021-06-28 13:20:45 -0600

    Creando formulario de pacientes

--- app/views/doctor/home.php
+++ app/views/doctor/home.php
@@ -8,7 +8,19 @@
     <title>Inicio Doctor</title>
 </head>
 <body>
-        <h1>Inicio del doctor</h1>
+        <h1>Inicio del doctor <?php echo $_SESSION["doctor"];?> </h1>
         <a href="">Cerrar Sesion</a>
+        <hr>
+        <h1>Añadir Paciente</h1>
+        <form action="home" method="post">
+            <input type="text" name="name" placeholder="Nombre" id="">
+            <input type="text" name="lastname" placeholder="Apellido" id="">
+            <input type="text" name="username" placeholder="Nombre de usuario" id="">
+            <input type="password" name="password" placeholder="Contraseña" id="">
+            <input type="email" name="email" placeholder="Email" id="">
+            <input type="text" name="dui" placeholder="Dui" id="">
+            <input type="number" name="age" placeholder="Edad" id="">
+            <input type="submit" value="Agregar paciente" name="register" id="">
+        </form>
 </body>
 </html>
