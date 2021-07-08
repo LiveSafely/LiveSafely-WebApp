48adacf16b259a125beb6cd9c9e8ef8f432f8ec4
dagr27 <00118717@uca.edu.sv>
2021-07-07 18:04:33 -0600

    Añadiendo landing page sin información

--- app/views/home/home.php
+++ app/views/home/home.php
@@ -6,10 +6,85 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LiveSafely</title>
+    <link href="assets/css/main.css" rel="stylesheet">
 </head>
-<body>
-    <h1>Inicio</h1>
-
-    <a href="home/login">Inicia Sesion</a>
+<body id="page-top">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+        <div class="container px-4">
+            <a class="navbar-brand" href="#page-top">LiveSafely</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span
+                    class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="#about">Acerca de</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#services">Servicios</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Contacto</a></li>
+                    <li class="nav-item"><a class="nav-link" href="home/login">Iniciar Sesion</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Titulo-->
+    <header class="bg-home text-white vh-100 d-flex align-items-center">
+        <div class="tela"></div>
+        <div class="container px-4 text-center position-relative">
+            <h1 class="fw-bolder">¡Bienvenidos a LiveSafely!</h1>
+            <p class="lead">La web app que te protege a ti y a los tuyos</p>
+            <a class="btn btn-lg btn-success" href="#about">Conoce un poco más...</a>
+        </div>
+    </header>
+    <!-- Acerca de-->
+    <section id="about">
+        <div class="container px-4">
+            <div class="row gx-4 justify-content-center">
+                <div class="col-lg-8">
+                    <h2>About this page</h2>
+                    <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum deserunt accusamus obcaecati ducimus in, dolores velit, assumenda cupiditate voluptatibus numquam recusandae laborum officia quisquam iste iusto. Dicta ab ipsum beatae.</p>
+                    <ul>
+                        <li>Clickable nav links that smooth scroll to page sections</li>
+                        <li>Responsive behavior when clicking nav links perfect for a one page website</li>
+                        <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the
+                            navbar</li>
+                        <li>Minimal custom CSS so you are free to explore your own unique design options</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Seccion de servicios -->
+    <section class="bg-light" id="services">
+        <div class="container px-4">
+            <div class="row gx-4 justify-content-center">
+                <div class="col-lg-8">
+                    <h2>Services we offer</h2>
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore,
+                        expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda
+                        laborum vel, labore ut velit dignissimos.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Seccion de contacto-->
+    <section id="contact">
+        <div class="container px-4">
+            <div class="row gx-4 justify-content-center">
+                <div class="col-lg-8">
+                    <h2>Contact us</h2>
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat
+                        voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et
+                        repudiandae ipsa exercitationem, in, quo totam.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer-->
+    <footer class="py-5 bg-dark">
+        <div class="container px-4">
+            <p class="m-0 text-center text-white">LiveSafelyWebApp</p>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/scripts.js"></script>
 </body>
 </html>
